20bd5f34c84e31d7fdc33e05f7cc1501c873ef69
deviloon <deviloon@yandex.ru>
2026-07-27 22:27:54 +0300

    Сделал кетбуст

--- models/03_final_compare.py
+++ models/03_final_compare.py
@@ -76,26 +76,145 @@ scoring = {
 }
 
 
-run_name = 'lightgbm_final'
-with mlflow.start_run(run_name=run_name):
-    print(f'Начало большого поиска Optuna для LightGBM...')
-    def objective(trial):
-        lgb_params = {
-            'learning_rate': trial.suggest_float('learning_rate', 0.005, 0.2, log=True),
-            'num_leaves': trial.suggest_int('num_leaves', 15, 512),
-            'min_child_samples': trial.suggest_int('min_child_samples', 10, 300),
-            'subsample': trial.suggest_float('subsample', 0.4, 1.0),
-            'colsample_bytree': trial.suggest_float('colsample_bytree', 0.4, 1.0),
-            'n_estimators': trial.suggest_int('n_estimators', 400, 1500, step=100),
-            'objective': 'mae',
-            'random_state': CONFIG['RANDOM_STATE'],
-            'n_jobs': 1, 
-            'verbose': -1,
-            'subsample_freq': 1
-        }
+# run_name = 'lightgbm_final'
+# with mlflow.start_run(run_name=run_name):
+#     print(f'Начало большого поиска Optuna для LightGBM...')
+#     def objective(trial):
+#         lgb_params = {
+#             'learning_rate': trial.suggest_float('learning_rate', 0.005, 0.2, log=True),
+#             'num_leaves': trial.suggest_int('num_leaves', 15, 512),
+#             'min_child_samples': trial.suggest_int('min_child_samples', 10, 300),
+#             'subsample': trial.suggest_float('subsample', 0.4, 1.0),
+#             'colsample_bytree': trial.suggest_float('colsample_bytree', 0.4, 1.0),
+#             'n_estimators': trial.suggest_int('n_estimators', 400, 1500, step=100),
+#             'objective': 'mae',
+#             'random_state': CONFIG['RANDOM_STATE'],
+#             'n_jobs': 1, 
+#             'verbose': -1,
+#             'subsample_freq': 1
+#         }
         
-        # Создаем новую модель для каждого триала
-        current_model = lgb.LGBMRegressor(**lgb_params)
+#         # Создаем новую модель для каждого триала
+#         current_model = lgb.LGBMRegressor(**lgb_params)
+#         current_wrapped = TransformedTargetRegressor(
+#             regressor=current_model,
+#             func=np.log1p,
+#             inverse_func=np.expm1
+#         )
+        
+#         scores = cross_validate(
+#             estimator=current_wrapped,
+#             X=X_train,
+#             y=y_train,
+#             scoring=scoring,
+#             cv=cv,
+#             n_jobs=-1
+#         )
+        
+#         mape = -scores['test_mape'].mean()
+#         mae = -scores['test_mae'].mean()
+#         trial.set_user_attr('mae', mae)
+        
+#         return mape
+
+
+
+#     start_time = time.time()
+#     optuna.logging.set_verbosity(optuna.logging.INFO)
+#     sampler = optuna.samplers.TPESampler(seed=CONFIG['RANDOM_STATE'])
+#     study = optuna.create_study(direction='minimize', sampler=sampler)
+#     study.optimize(objective, n_trials=150)
+#     time_elapsed = time.time() - start_time
+#     print(f"Поиск завершен за {time_elapsed / 60:.2f} минут.")
+
+#     best_cv_mape = study.best_value
+#     best_cv_mae = study.best_trial.user_attrs['mae']
+#     best_params = study.best_params
+
+
+
+
+#     print("Начало финального обучения на всех данных...")
+#     start_time_fit = time.time()
+    
+#     # Докидываем к лучшим параметрам константы, чтобы собрать финальную модель
+#     final_lgb_params = best_params.copy()
+#     final_lgb_params.update({
+#         'objective': 'mae',
+#         'random_state': CONFIG['RANDOM_STATE'],
+#         'n_jobs': -1, # Тут можно -1, т.к. мы больше не делаем кросс-валидацию параллельно
+#         'verbose': -1,
+#         'subsample_freq': 1
+#     })
+#     final_model = lgb.LGBMRegressor(**final_lgb_params)
+#     final_wrapped = TransformedTargetRegressor(
+#         regressor=final_model,
+#         func=np.log1p,
+#         inverse_func=np.expm1
+#     )
+    
+#     final_wrapped.fit(X_train, y_train)
+    
+#     time_fit = time.time() - start_time_fit
+    
+#     print("Проверка на X_test...")
+#     y_pred_test = final_wrapped.predict(X_test)
+    
+#     test_mape = mean_absolute_percentage_error(y_test, y_pred_test)
+#     test_mae = mean_absolute_error(y_test, y_pred_test)
+
+    
+#     mlflow.log_params({f"regressor__{k}": v for k, v in final_lgb_params.items()})
+    
+#     # Метрики кросс-валидации (какое качество модель ожидала)
+#     mlflow.log_metric('cv_best_mape', best_cv_mape)
+#     mlflow.log_metric('cv_best_mae', best_cv_mae)
+    
+#     # МЕТРИКИ НА ТЕСТЕ (РЕАЛЬНОЕ КАЧЕСТВО НА НОВЫХ ДАННЫХ)
+#     mlflow.log_metric('test_mape', test_mape)
+#     mlflow.log_metric('test_mae', test_mae)
+    
+#     # Время
+#     mlflow.log_metric('time_hpo_sec', time_elapsed)
+#     mlflow.log_metric('time_final_fit_sec', time_fit)
+    
+#     mlflow.sklearn.log_model(final_wrapped, "best_lightgbm_model")
+
+#     print(f"Готово!")
+#     print(f"CV MAPE: {best_cv_mape:.4f} | TEST MAPE (Итог): {test_mape:.4f}")
+#     print(f"CV MAE:  {best_cv_mae:.0f} | TEST MAE (Итог):  {test_mae:.0f}")
+
+
+run_name = 'catboost_final'
+with mlflow.start_run(run_name=run_name):
+    cat_features = X_train.select_dtypes(include=['object', 'category']).columns.to_list()
+    print(f'Начало большого поиска Optuna для CatBoost...')
+    def objective(trial):
+        bootstrap_type = trial.suggest_categorical('bootstrap_type', ['Bayesian', 'Bernoulli', 'MVS'])
+        cb_params = {
+        'iterations': trial.suggest_int('iterations', 600, 2500, step=100),
+        'learning_rate': trial.suggest_float('learning_rate', 0.005, 0.15, log=True),
+        'depth': trial.suggest_int('depth', 4, 10),
+        'l2_leaf_reg': trial.suggest_float('l2_leaf_reg', 1e-2, 20.0, log=True),
+        'random_strength': trial.suggest_float('random_strength', 1e-3, 10.0, log=True),
+        'border_count': trial.suggest_int('border_count', 32, 255),
+        'min_data_in_leaf': trial.suggest_int('min_data_in_leaf', 10, 200),
+        'rsm': trial.suggest_float('rsm', 0.5, 1.0),
+        'bootstrap_type': bootstrap_type,
+        'loss_function': 'MAE',
+        'eval_metric': 'MAE',
+        'random_seed': CONFIG['RANDOM_STATE'],
+        'thread_count': 1,
+        'verbose': 0
+        }
+        if bootstrap_type == 'Bayesian':
+            cb_params['bagging_temperature'] = trial.suggest_float('bagging_temperature', 0.0, 10.0)
+        elif bootstrap_type in ['Bernoulli', 'MVS']:
+            cb_params['subsample'] = trial.suggest_float('subsample', 0.4, 1.0)
+
+
+        
+        current_model = CatBoostRegressor(**cb_params)
         current_wrapped = TransformedTargetRegressor(
             regressor=current_model,
             func=np.log1p,
@@ -108,6 +227,7 @@ with mlflow.start_run(run_name=run_name):
             y=y_train,
             scoring=scoring,
             cv=cv,
+            params={'cat_features': cat_features},
             n_jobs=-1
         )
         
@@ -136,24 +256,22 @@ with mlflow.start_run(run_name=run_name):
 
     print("Начало финального обучения на всех данных...")
     start_time_fit = time.time()
-    
-    # Докидываем к лучшим параметрам константы, чтобы собрать финальную модель
-    final_lgb_params = best_params.copy()
-    final_lgb_params.update({
-        'objective': 'mae',
-        'random_state': CONFIG['RANDOM_STATE'],
-        'n_jobs': -1, # Тут можно -1, т.к. мы больше не делаем кросс-валидацию параллельно
-        'verbose': -1,
-        'subsample_freq': 1
+    final_catboost_params = best_params.copy()
+    final_catboost_params.update({
+        'loss_function': 'MAE',
+        'eval_metric': 'MAE',
+        'random_seed': CONFIG['RANDOM_STATE'],
+        'thread_count': -1,
+        'verbose': 0
     })
-    final_model = lgb.LGBMRegressor(**final_lgb_params)
+    final_model = CatBoostRegressor(**final_catboost_params)
     final_wrapped = TransformedTargetRegressor(
         regressor=final_model,
         func=np.log1p,
         inverse_func=np.expm1
     )
     
-    final_wrapped.fit(X_train, y_train)
+    final_wrapped.fit(X_train, y_train, cat_features=cat_features)
     
     time_fit = time.time() - start_time_fit
     
@@ -164,21 +282,22 @@ with mlflow.start_run(run_name=run_name):
     test_mae = mean_absolute_error(y_test, y_pred_test)
 
     
-    mlflow.log_params({f"regressor__{k}": v for k, v in final_lgb_params.items()})
+    mlflow.log_params({f"regressor__{k}": v for k, v in final_catboost_params.items()})
     
-    # Метрики кросс-валидации (какое качество модель ожидала)
     mlflow.log_metric('cv_best_mape', best_cv_mape)
     mlflow.log_metric('cv_best_mae', best_cv_mae)
     
-    # МЕТРИКИ НА ТЕСТЕ (РЕАЛЬНОЕ КАЧЕСТВО НА НОВЫХ ДАННЫХ)
     mlflow.log_metric('test_mape', test_mape)
     mlflow.log_metric('test_mae', test_mae)
     
-    # Время
-    mlflow.log_metric('time_hpo_sec', time)
+    mlflow.log_metric('time_hpo_sec', time_elapsed)
     mlflow.log_metric('time_final_fit_sec', time_fit)
     
-    mlflow.sklearn.log_model(final_wrapped, "best_lightgbm_model")
+    mlflow.sklearn.log_model(
+        final_wrapped, 
+        artifact_path="best_catboost_model",
+        serialization_format="cloudpickle"
+    )
 
     print(f"Готово!")
     print(f"CV MAPE: {best_cv_mape:.4f} | TEST MAPE (Итог): {test_mape:.4f}")
